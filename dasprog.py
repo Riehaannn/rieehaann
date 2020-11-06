66ba12183c53772e001c207733e0d9e66fef1c89
imronassidiqi <73922408+imronassidiqi@users.noreply.github.com>
2020-11-06 09:52:39 +0700

    Update dasprog.py

--- dasprog.py
+++ dasprog.py
@@ -1,1 +1,25 @@
-print("semarang")
+#PROGRAM MENGHITUNG LUAS DAN KELILING BANGUN DATAR
+#DISUSUN OLEH ATALARIC REIHAN DAN IMRON ASSIDIQI
+from IPython.display import clear_output
+def cls():
+  clear_output(True)
+  
+while True:
+  #menu pilihan
+  print("pilihan program"_)
+  print("1. Luas Persegi")
+  print("2. Luas Persegi Panjang")
+  print("3. Luas Segitiga")
+  print("4. Luas Lingkaran")
+  print("5. Keliling Persegi")
+  print("6. Keliling Persegi Panjang")
+  print("7. Keliling Segitiga")
+  print("8. Keliling Lingkaran")
+  print("9. Keluar")
+  
+  #input
+  pilih = int(input("Masukkan Pilihan : "))
+  
+  cls()
+  
+  if
